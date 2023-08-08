86a97f8fde7de4406201499337e1b69644492b08
SafaFarah <kangjisook13@gmail.com>
2023-08-08 21:02:43 +0000

    task 6 add 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print('{0}{1}, '.format(i, j), end="")
         if i == 8 and j == 9:
-            print('{0}{1}'.format(i, j))
+            break
+        print('{0}{1}, '.format(i, j), end="")
+print('{0}{1}'.format(i, j))
