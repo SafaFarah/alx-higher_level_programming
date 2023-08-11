5e7a33d63399b177ae8fc78c3ef1c3b30a8d4517
SafaFarah <kangjisook13@gmail.com>
2023-08-11 09:55:04 +0000

    add 3-infinite_add.py file

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    result = 0
+    for i in range(1, len(argv)):
+        result = result + int(argv[i])
+    print (result)
+    main()
