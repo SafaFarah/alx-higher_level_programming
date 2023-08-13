355a614c7b4e540f8a11bff3b9f189e9b9fa43c8
SafaFarah <kangjisook13@gmail.com>
2023-08-13 08:03:12 +0000

    debug task 5 again and again

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    if my_string is not None:
-        s_list = list(my_string)
-        for ch in s_list:
-            if ch == 'c' or ch == 'C':
-                s_list.remove(ch)
+    s_list = list(my_string)
+    for ch in s_list:
+        if ch == 'c' or ch == 'C':
+            s_list.remove(ch)
     return("".join(s_list))
