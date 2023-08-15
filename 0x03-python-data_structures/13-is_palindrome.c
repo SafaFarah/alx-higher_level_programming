def2a451a73af5dff977191474aa3a431bbb53b0
SafaFarah <kangjisook13@gmail.com>
2023-08-15 22:45:24 +0000

    debugging 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 
 /**
+ * isPalindrome - checks if a singly linked lis    t is a palindrome.
+ * @start: pointer to pointer to head of list.
+ * @end:  pointer to head of list.
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindr    ome.
+ */
+int isPalindrome(listint_t **start, listint_t *end)
+{
+	int r;
+	int r1;
+
+	if (end == NULL)
+		return (1);
+	r = isPalindrome(start, end->next);
+	if (r == 0)
+		return (0);
+	if (end->n == (*start)->n)
+		r1 = 1;
+	else
+		r1 = 0;
+	*start = (*start)->next;
+	return (r1);
+}
+
+/**
  * is_palindrome -  a function checks if a singly linked list is a palindrome.
  * @head: pointer to pointer to head of list.
  *
@@ -9,32 +34,5 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *start;
-	listint_t *end;
-	listint_t *temp_e;
-
-	if (*head == NULL)
-		return (1);
-	end = *head;
-	start = *head;
-	while (end->next != NULL)
-	{
-		end = end->next;
-	}
-	while (start->next != temp_e && start->next != NULL)
-	{
-		if (end->n == start->n)
-		{
-			temp_e = end;
-			end = start->next;
-			while (end->next != temp_e)
-			{
-				end = end->next;
-			}
-			start = start->next;
-		}
-		if (end->n != start->n)
-			return (0);
-	}
-	return (1);
+	return (isPalindrome(head, *head));
 }
