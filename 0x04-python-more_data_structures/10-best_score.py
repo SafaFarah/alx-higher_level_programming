2c9e53d9cc7953d87c174f923f18810503c1b8ea
SafaFarah <kangjisook13@gmail.com>
2023-08-16 12:48:13 +0000

    debugging task 5-6

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,8 @@ def best_score(a_dictionary):
         return None
     keys = list(a_dictionary.keys())
     best = keys[0]
+    if best is None:
+        return None
     for i in range(1, len(keys)):
         if a_dictionary[keys[i]] > a_dictionary[best]:
             best = keys[i]
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sort_dic = sorted(a_dictionary.keys())
     for key in sort_dic:
