24aacbdd453e6cdba5b8ad60b22e7817df7b33f8
SafaFarah <kangjisook13@gmail.com>
2023-08-29 23:29:28 +0000

    solving task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Class Square that defines a square."""
+
+
+class Square:
+    """
+    An empty class Square that defines a square.
+
+    Attributes:
+        size: The size of a square.
+    """
+    def __init__(self, size=0):
+        """
+        The constructor for Square class.
+
+        Parameters:
+            size(int): The size of a square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Public instance method  returns the current square size.
+
+        Returns: The current square size.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        Public instance method  sets the square size.
+
+        Paramerters:
+            value: new size to set size
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Public instance method  returns the current square area.
+
+        Returns: The current square area.
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+
+        """Print the square with the # character."""
+
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
