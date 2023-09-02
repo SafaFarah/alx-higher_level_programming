24901246c81c8db37325a9ab99a74ce01dff57c9
SafaFarah <kangjisook13@gmail.com>
2023-09-02 07:55:42 +0000

    adding 100-print_python_list_info.c file

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include <python.h>
 
+/**
+ * print_python_list_info -  print information about pyobject.
+ * @p:  pyobject
+ *
+ */
 void print_python_list_info(PyObject *p)
 {
+	int n;
 	int size;
 
-	size = (((PyVarObject*)(p))->ob_size);
-	printf("[*] Size of the Python List = %d", size)
+	size = (PyVarObject *)p->ob_size;
+	printf("[*] Size of the Python List = %d", size);
+	printf("[*] Allocated = %d", (PyVarObject *)p->allocated);
+	for (n = 0; n < size; n++)
+		printf("Element %d: %s\n", n, (PyVarObject *)p->ob_item[n]->ob_type);
 }
-
