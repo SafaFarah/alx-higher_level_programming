eb3de25e088e53bee6edc7414b43ca6f5b28bcbf
SafaFarah <kangjisook13@gmail.com>
2023-09-04 17:35:44 +0000

    adding 2 methods returns the rectangle perimeter and area

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""Class Rectangle that defines a rectangle."""
+
+
+class Rectangle:
+    """
+    A class Rectangle that defines a Rectangle.
+
+    Attributes:
+        width: The width  of a rectangle
+        width: The height  of a rectangle.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        The constructor for rectangle  class.
+
+        Parameters:
+            width: The width  of a rectangle.
+            width: The height  of a rectangle.
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        Public instance method  returns the current rectangle width.
+        Returns: The current rectangle width.
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """
+        Public instance method  sets the rectangle width..
+        Paramerters:
+            value: new width to set width
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+            self.__position = position
+
+    @property
+    def height(self):
+        """
+        Public instance method  returns the current rectangle height.
+
+        Returns: The current rectangle height.
+        """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """
+        Public instance method  sets the rectangle height.
+
+        Paramerters:
+            value: new size to set rectangle height.
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Public instance method  returns the current rectangle  area.
+        Returns: The current rectangle area.
+        """
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """
+        Public instance method  returns the current rectangle  perimeter.
+        Returns: The current rectangle  perimeter.
+        """
+        if (width == 0  or height == 0)
+            return 0
+        else
+            return 2 * (self.__width + self.__height)
