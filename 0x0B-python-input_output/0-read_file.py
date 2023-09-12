be6502c04e460175ca612487242bd0835a7c47a9
SafaFarah <kangjisook13@gmail.com>
2023-09-12 09:48:06 +0000

    debugging  0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,6 @@ def read_file(filename=""):
     Args:
     filename: a text file.
     """
-    with open(filename, 'r', encoding="UTF8",) as f:
+    with open(filename, 'r', encoding="UTF8") as f:
         read_data = f.read()
         print(read_data)
