34d1fd7dbe6f60a429efe87b81c482d43516d9ea
SafaFarah <kangjisook13@gmail.com>
2023-09-12 21:04:51 +0000

    create 7-add_item.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines  a function that creates an Object
+from a JSON file.
+"""
+
+
+def load_from_json_file(filename):
+    """
+    a function that creates an Object
+    from a JSON file.
+    Args:
+        filename: a JSON file.
+    """
+    import json
+    with open(filename, "r", encoding="utf-8") as f:
+        return (json.load(f))
