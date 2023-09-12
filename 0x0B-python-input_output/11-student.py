8313302d6b41e07f99ae1a22e961b092de77e39f
SafaFarah <kangjisook13@gmail.com>
2023-09-12 22:29:55 +0000

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Defines a class Student defines a student """
+
+
+class Student:
+    """
+    A class Student that defines a student by:
+    Public instance attributes:
+        first_name - last_name - age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Creates new object of Student.
+        Args:
+            first_name: first name of student.
+            last_name: last name of student.
+            age: age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation
+        of a Student instance.
+        Return:
+             the dictionary description
+        """
+        if attrs is None:
+            return self.__dict__
+
+        _dict = {}
+        for i in attrs:
+            try:
+                _dict[i] = self.__dict__[i]
+            except Exception:
+                pass
+        return _dict
+        
+    def reload_from_json(self, json):
+        """ Public method replaces all
+        attributes of the Student instance.
+
+        Args:
+            json: object.
+        """
+        self.__dict__.update(json)
