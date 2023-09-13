0f40a34301a5104e1fa784d20afbd673f2ad70cf
SafaFarah <kangjisook13@gmail.com>
2023-09-13 15:58:04 +0000

    100-print_python_list_info.c debugging

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -15,9 +15,9 @@ void print_python_list_info(PyObject *p)
 	pylist = (PyListObject *)p;
 	size = pylist->ob_base.ob_size;
 	printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", pylist->allocated);
-    for (n = 0; n < size; i++)
-    {
-        printf("Element %d: %s\n", n, pylist->ob_item[n]->ob_type->tp_name);
-    }
+	printf("[*] Allocated = %ld\n", pylist->allocated);
+	for (n = 0; n < size; n++)
+	{
+		printf("Element %d: %s\n", n, pylist->ob_item[n]->ob_type->tp_name);
+	}
 }
