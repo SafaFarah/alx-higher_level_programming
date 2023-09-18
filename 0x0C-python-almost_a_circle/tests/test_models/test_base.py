07acc8fc1f4a8c2994d55abeefeb4fd92f3211ec
SafaFarah <kangjisook13@gmail.com>
2023-09-18 07:53:12 +0000

    test_rectangle.py add type test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,18 +2,29 @@ import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
+    """ 
+    Test: Square class to test Base model
+    """
 
     @classmethod
     def setUpClass(cls):
+        """A class method called before tests
+        in an individual class run. """
         pass
 
     @classmethod
     def tearDownClass(cls):
+        """ A class method called after tests
+        in an individual class have run."""
         pass
 
     def setUp(self):
-        pass
-
-
+        """Method called to prepare the test
+        fixture. """
+        Base._Base__nb_objects = 0
+    
     def tearDown(self):
+        """Method called after calling the
+        test method and the result recorded.
+        """
         pass
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -15,29 +15,39 @@ class TestRectangle(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """ """
+        """A class method called before tests
+        in an individual class run. """
         pass
 
     @classmethod
     def tearDownClass(cls):
-        """ """
+        """ A class method called after tests
+        in an individual class have run."""
         pass
 
     def setUp(self):
-        """ """
+        """Method called to prepare the test
+        fixture. """
         Base._Base__nb_objects = 0
         self.r1 = Rectangle(10, 2)
         self.r2 = Rectangle(10, 2, 0, 0, 12)
         self.r3 = Rectangle(8, 7, 0, 0, 12)
 
     def tearDown(self):
-        """ """
+        """Method called after calling the
+        test method and the result recorded.
+        """
         pass
 
     def test_Rect_inherit(self):
         """ Test: Rectangle is sub class of
         Base """
         self.assertTrue(issubclass(Rectangle, Base))
+
+    def test_type(self):
+        """ Test: Rectangle class type """
+        t = "<class 'models.rectangle.Rectangle'>"
+        self.assertEqual(str(Rectangle), t)
 
     def test_attrs(self):
         """ Test: Rectangle class  attributes """
@@ -73,13 +83,22 @@ class TestRectangle(unittest.TestCase):
         """ Test: number of arguments """
         with self.assertRaises(TypeError) as e:
             r5 = Rectangle()
-        exp1 = "Rectangle.__init__() missing 2 required positional arguments: 'width' and 'height'"
+        exp_1 = "Rectangle.__init__() missing 2 required positional arguments:"
+        exp11 = " 'width' and 'height'"
+        exp1 = exp_1 + exp11
         self.assertEqual(str(e.exception), exp1)
-
+        with self.assertRaises(TypeError) as e:
+            r9 = Rectangle(2)
+        exp22 = "Rectangle.__init__() missing 1"
+        exp_2 = " required positional argument: 'height'"
+        exp2 = exp22 + exp_2
+        self.assertEqual(str(e.exception), exp2)
         with self.assertRaises(TypeError) as e:
             r6 = Rectangle(1, 3, 2, 5, 9, 8)
-        exp2 = "Rectangle.__init__() takes from 3 to 6 positional arguments but 7 were given"
-        self.assertEqual(str(e.exception), exp2)
+        exp33 = "Rectangle.__init__() takes from 3 to 6"
+        exp_3 = " positional arguments but 7 were given"
+        exp3 = exp33 + exp_3
+        self.assertEqual(str(e.exception), exp3)
 
     def test_area(self):
         """ Test: The area of Rectangle"""
@@ -104,8 +123,10 @@ class TestRectangle(unittest.TestCase):
             self.assertEqual(out.getvalue(), rect)
 
     def test_to_dictionary(self):
-        """ Test: method returns the dictionary representation of a Rectangle"""
-        self.assertEqual(self.r3.to_dictionary(), {'x': 0, 'y': 0, 'id': 12, 'height': 7, 'width': 8})
+        """ Test:Method returns the dictionary representation of a Rectangle"""
+        dic = {'x': 0, 'y': 0, 'id': 12, 'height': 7, 'width': 8}
+        self.assertEqual(self.r3.to_dictionary(), dic)
+        self.assertEqual(type(dic), dict)
 
     def update(self, *args, **kwargs):
         """Test: assigns a key/value argument to attributes"""
