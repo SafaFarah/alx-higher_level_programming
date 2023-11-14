1887be81a97db3907ab4db6b09870de1d585902b
SafaFarah <122905189+SafaFarah@users.noreply.github.com>
2023-11-14 22:04:53 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-let number;
-number = parseInt(process.argv[2]);
-if (!isNaN(number)) {
-  const string = 'My number: ' + number;
-  console.log(string);
-} else {
-  console.log('Not a number');
-}
