af7f4213288c17af8030f0857eb10120aa92ed66
Safa Farah <kangjisook13@gmail.com>
2024-01-15 17:19:31 +0200

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
-import MySQLdb
+
 from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
