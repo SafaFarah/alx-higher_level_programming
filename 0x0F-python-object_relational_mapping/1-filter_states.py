15901d6c82cdc5461c0e050c2dba1ad356aee03d
Safa Farah <kangjisook13@gmail.com>
2024-01-15 19:52:53 +0200

    debug task 1 2 3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,12 +4,12 @@ a script that lists all states from the database hbtn_0e_0_usa
 """
 from sys import argv
 import MySQLdb
-if __name__ == " __main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          password=argv[2], database=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'
-                ORDER BY states.id ASC")
+    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+                ORDER BY states.id ASC""")
     row = cur.fetchall()
     for i in row:
         print(i)
