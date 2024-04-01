edd23f1f298f6e747c5ac9b3f66a3e1266555628
SafaFarah <kangjisook13@gmail.com>
2024-04-01 13:51:47 +0200

    create 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines a function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """
+    A function that returns an object
+    (Python data structure) represented by
+    a JSON string.
+    Args:
+        list_of_integers: list of integers
+    Return:
+        a peak in a list of intergers
+    """
